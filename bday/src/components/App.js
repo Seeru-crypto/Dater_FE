5ef536c5e00744b9458d1f81c2b9ba7adc6b997d
Seeru-crypto <76745760+Seeru-crypto@users.noreply.github.com>
2021-08-12 20:40:06 +0300

    added routing and dashboard placeholder

--- bday/src/components/App.js
+++ bday/src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+// npm install react-router-dom
+
 
 import Testing from "./Testing"; 
 import BirthdayForm from "./BirthdayForm";
+import Dashboard from './Dashboard';
 
 const App = () => {
 
@@ -10,8 +14,13 @@ const App = () => {
 
   return (
       <div className="container">
-          <p>In App container</p>
-          <BirthdayForm />
+        <Router>
+          <Switch>
+          <Route exact path="/create" component={ Dashboard } />
+          <Route exact path="/" component={ BirthdayForm } />
+          </Switch>
+          </Router>
+
       </div>
   )
 }
