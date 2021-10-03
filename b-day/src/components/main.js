d7c0f0a89a43d841cbf98fef209776c01bc52537
Seeru <iBlueman260@gmail.com>
2021-10-03 15:44:49 +0300

    GET works reasnobly well

--- b-day/src/components/main.js
+++ b-day/src/components/main.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Service from '../service';
+import React, { useState, useEffect } from 'react';
+//import { getUsers } from '../service';
+import useGetData from './API/get-data';
 
 const Main = () => {
-    const { resData, isPending, error } = Service.getUsers(
-        'http://localhost:3000/users'
-    );
-    console.log(resData);
-    console.log(isPending);
-    console.log(error);
+    const [test, setTest] = useState('');
+    const [test2, setTest2] = useState('');
 
-    return <div>Main page!</div>;
+    //const service = new Service();
+
+    const { data, isPending, error } = useGetData(
+        `http://localhost:3000/users`
+    );
+
+    useEffect(() => {
+        //setTest(getUsers('http://localhost:3000/users'));
+    }, []);
+
+    return <div>{data && data.data.map((entry) => <p>{entry.name}</p>)}</div>;
 };
 export default Main;
--- b-day/src/service.js
+++ b-day/src/service.js
@@ -1,86 +1,54 @@
 import axios from 'axios';
 
-class Service {
-    getUsers(REST_API_URL) {
-        console.log('in service ', REST_API_URL);
-        let resData = null;
-        let isPending = null;
-        let error = null;
-        axios
-            .get(REST_API_URL)
-            .then((res) => {
-                if (res.statusText !== '') {
-                    throw Error('could not fetch the data');
-                } // adds a error check, so that if the server exists but the endpoint is not available/ down then throw an error
-                // return res.json();
-                return res;
-            })
-            .then((data) => {
-                console.log('data is ', data);
-                resData = data;
-                isPending = false;
-                error = null;
-            });
+export const getUsers = (REST_API_URL) => {
+    const data = axios.get(REST_API_URL);
+    return data;
+    //console.log('data is service ', data);
+};
 
-        /*const abortCont = new AbortController();
-        axios.get(REST_API_URL, {signal:abortCont.signal})
-          .then(res => {
-            if(res.statusText !== "") {throw Error("could not fetch the data")}  // adds a error check, so that if the server exists but the endpoint is not available/ down then throw an error
-            // return res.json();
-            return res;
-          })
-          .then(data => {
-            console.log('data is ', data)
-            resData = (data);
-            isPending = (false);
-            error = (null);
-          })
-          .catch( err => {
-            if (err.name==="AbortError"){
-              console.log("fetch aborted");
-            }else {
-              error = (err.message);
-              isPending = (false);
-            }
-          })  // it will catch network errors/ cant connect to the server at all
-    console.log(resData);*/
+export const getUserByID = (REST_API_URL, id) => {
+    const path = `${REST_API_URL}/${id}`;
+    let resData = null;
+    let isPending = null;
+    let error = null;
+    axios.get(path).then((data) => {
+        resData = data.data;
+        isPending = false;
+        //console.log('in service ', resData);
+        error = null;
         return { resData, isPending, error };
-    }
+    });
+    //console.log('in service ', resData);
+    //return { resData, isPending, error };
+};
 
-    getUserByID(REST_API_URL, id) {
-        const path = REST_API_URL + id;
-        axios.get(path).then(function (response) {
-            return response;
-        });
-    }
-    postNewUser(REST_API_URL, storage) {
-        axios.post(REST_API_URL, storage).then((res) => {
-            return res;
-        });
-    }
-    deleteUser(REST_API_URL, item_id) {
-        const path = REST_API_URL + '/' + item_id;
-        axios.delete(path).then(function (response) {
-            console.log(response);
-            return response;
-        });
-    }
-    Delete(URL, item_id) {
-        const path = URL + item_id;
-        axios.delete(path).then(() => {
-            window.location.reload();
-        });
-    }
-    PUT(URL, id, data) {
-        const path = URL + id;
-        console.log('data is');
-        console.log(data);
+export const postNewUser = (REST_API_URL, storage) => {
+    axios.post(REST_API_URL, storage).then((res) => {
+        return res;
+    });
+};
 
-        console.log('Put path is ', path);
-        axios.put(path, data).then(() => {
-            window.location.reload();
-        });
-    }
-}
+export const deleteUser = (REST_API_URL, item_id) => {
+    const path = REST_API_URL + '/' + item_id;
+    axios.delete(path).then(function (response) {
+        console.log(response);
+        return response;
+    });
+};
 
-export default new Service();
+export const Delete = (URL, item_id) => {
+    const path = URL + item_id;
+    axios.delete(path).then(() => {
+        window.location.reload();
+    });
+};
+
+export const PUT = (URL, id, data) => {
+    const path = URL + id;
+    console.log('data is');
+    console.log(data);
+    console.log('Put path is ', path);
+    axios.put(path, data).then(() => {
+        window.location.reload();
+    });
+};
