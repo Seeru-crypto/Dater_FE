c14c8fec21964dd040ef01345781df20a48c59b9
Fred <iblueman260@gmail.com>
2021-11-02 20:46:25 +0200

    clean code

--- src/components/API/api-requests.js
+++ src/components/API/api-requests.js
@@ -7,26 +7,22 @@ export const DeleteData = async (url, id) => {
 }
 
 export const GetData = async (url) => {
-    const axios = require('axios').default
     const res = await axios.get(url)
     return res
 }
 
 export const GetDataById = async (url, id) => {
     const path = (url, '/' + id)
-    const axios = require('axios').default
     const res = await axios.get(path)
     return res
 }
 
 export const PostData = async (url, data) => {
-    const axios = require('axios').default
     const res = axios.post(url, data)
     return res
 }
 
 export const UpdateData = async (url, data) => {
-    const axios = require('axios').default
     const res = axios.put(url, data)
     return res
 }
