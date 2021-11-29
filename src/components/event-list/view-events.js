b3e4180ab720ed2e408b3e7ee50e6a4b9ba3008b
Seeru <iBlueman260@gmail.com>
2021-11-29 19:59:38 +0200

    deploy-prep: add check email button

--- src/components/event-list/view-events.js
+++ src/components/event-list/view-events.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Message } from 'primereact/message'
+import { Button } from 'primereact/button'
 
 import FilterTable from './filter-table'
 import config from '../../config.json'
@@ -8,19 +9,26 @@ import useGetData from '../../API/useGetData'
 const ViewEvents = () => {
     const apiPath = 'http://localhost:8080/api/event'
     const defaultErrorMessage = config.labels.defaultErrorMessage
-
     let { getData: data, isPending, error } = useGetData(apiPath)
-
-    console.log('isPending is ', isPending)
-
     return (
         <div>
             <div
                 hidden={error ? true : false}
                 style={{ padding: '1rem' }}
-                className="p-grid"
+                className="p-grid vertical-container"
             >
-                <h5 style={{ paddingRight: '2rem' }}>View events: </h5>
+                <h5 className="p-col p-col-align-start">View events: </h5>
+                <div>
+                    <div className="p-col p-col-align-end">
+                        <Button
+                            onClick={() => console.log('email checking!')}
+                            className="p-button-outlined p-button-secondary"
+                        >
+                            <i className="pi pi-envelope p-px-2" />
+                            <span> Check dates </span>
+                        </Button>
+                    </div>
+                </div>
             </div>
             <div
                 hidden={error ? false : true}
