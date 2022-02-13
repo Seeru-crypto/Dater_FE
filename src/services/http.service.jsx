edaee5f0ec13cb0252f6d6fe439fb8e41edeb528
Fred-Andree Oja <fred-andree.oja@netgroup.com>
2022-02-13 15:13:34 +0200

    main: add API env variable

--- src/services/http.service.jsx
+++ src/services/http.service.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import config from "../config.json"
 
+const path = process.env.REACT_APP_TOKEN_API_PATH || config.API_PATH
+
 const http = axios.create({
-    baseURL: config.API_PATH,
+    baseURL: path,
     headers: {
         'Content-Type': 'application/json',
     }
